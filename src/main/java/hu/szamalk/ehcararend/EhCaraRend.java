4c06c43b8df59b20998dd36129ffa3f9cf30d4dc
Szabó Roland <szrtanul@gmail.com>
2025-03-14 14:35:56 +0100

    Heap Sort

--- src/main/java/hu/szamalk/ehcararend/EhCaraRend.java
+++ src/main/java/hu/szamalk/ehcararend/EhCaraRend.java
@@ -11,6 +11,38 @@ package hu.szamalk.ehcararend;
 public class EhCaraRend {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int[] items = new int[]{133,13,73,52,66,2,78,99,42,5};
+        megjelenit(items);
+        System.out.println("");
+        items = maxKupac(items);
+        System.out.println("");
+        megjelenit(items);
+    }
+    
+    public static void megjelenit(int[] items){
+        for (int item : items) {
+            System.out.println(item);
+        }
+    }
+    
+    public static int[] maxKupac(int[] items){
+        int temp;
+        for (int k = items.length, c = 1; k >= 0; k--, c++) {
+            for (int i = (k/2)-1; i >= 0; i--) {
+               // System.out.println("i: "+i);
+                for (int j = 0; j < 2; j++) {
+                    //System.out.println("j: " + j);
+                   // System.out.println("Keplet: " + (int)(2 * i + j));
+                    temp = items[i];
+                    boolean b = items[i] < items[2*i+j];
+                    items[i] = b ? items[2*i+j] : items[i];
+                    items[2*i+j] = b ? temp : items[2*i+j];
+                }
+                temp = items[0];
+                items[0] = items[items.length-c];
+                items[items.length-c] = temp;
+            }
+        }
+        return items;
     }
 }
---
+++ src/main/java/hu/szamalk/ehcararend/KupacRend.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hu.szamalk.ehcararend;
+
+/**
+ *
+ * @author SzabóRoland(SZF_2023
+ */
+public class KupacRend {
+    
+}
