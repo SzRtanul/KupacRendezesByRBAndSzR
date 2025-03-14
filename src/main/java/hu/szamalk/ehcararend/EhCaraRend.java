ec90656fdf5910080b168a5c89392be5c75aa496
Szabó Roland <szrtanul@gmail.com>
2025-03-14 13:46:45 +0100

    INIT

---
+++ src/main/java/hu/szamalk/ehcararend/EhCaraRend.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package hu.szamalk.ehcararend;
+
+/**
+ *
+ * @author SzabóRoland(SZF_2023
+ */
+public class EhCaraRend {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
